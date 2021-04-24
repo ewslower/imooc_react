75667c4c614b47f6436dd90c46b4cecd612c871a
Admin <wang@ecoiiot.com>
2021-04-24 16:05:34 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import PriceList from './components/PriceList'
+const items = [
+	{
+		"id": 1,
+		"title": "去云南旅游",
+		"price": 200,
+		"date": "2018-09-10",
+		"category": {
+			"id": "1",
+			"name": "旅行",
+			"type": "outcome"
+		}
+	},
+	{
+		"id": 2,
+		"title": "去云南旅游",
+		"price": 400,
+		"date": "2018-09-10",
+		"category": {
+			"id": "1",
+			"name": "旅行",
+			"type": "outcome"
+		}
+	}
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		<PriceList 
+			items={items}
+			onModifyItem={(item) => {alert(item.id)}}
+			onDeleteItem={(item) => {alert(item.id)}}
+		/>
     </div>
   );
 }

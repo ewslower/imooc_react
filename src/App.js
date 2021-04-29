bb36760a8929b7b31c21c43a7659854453cabaa1
Admin <wang@ecoiiot.com>
2021-04-29 16:59:41 +0800

    公司网址

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Home from './containers/Home'
+import Home from './components/home/x2'
+import About from './components/about/xe42edb53'
+import Contact from './components/contact/xab66a7dc'
+import Service from './components/service/x389cd3ef'
+import Shipping from './components/shipping/x93270faf'
+
+import { Route, BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-		<Home />
-    </div>
+	<BrowserRouter>
+		<Route exact path="/" component={Home} />
+		<Route exact path="/about" component={About} />
+		<Route exact path="/contact" component={Contact} />
+		<Route exact path="/service" component={Service} />
+		<Route exact path="/shipping" component={Shipping} />
+	</BrowserRouter >
   );
 }
 
